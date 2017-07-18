da3e4e61e600b4ad7f62de7f1d29ecad1ca549e2
Jan Timpe <jantimpe@email.uark.edu>
2017-07-18 06:08:20 -0500

    Added simple pagination

--- src/api/recipes.js
+++ src/api/recipes.js
@@ -4,15 +4,13 @@ export const HTTP = axios.create({
    baseURL: 'https://api.edamam.com',
    params: {
       app_id: 'e8149f6c',
-      app_key: '8237364c4dc4573c8a7da14f8f1e10c0',
-      from: 0,
-      to: 10
+      app_key: '8237364c4dc4573c8a7da14f8f1e10c0'
    }
 })
 
 export default {
-   searchRecipes(query, healthLabel, dietLabel, onSuccess, onFailure) {
-      var resource = '/search?q='+query
+   searchRecipes(query, healthLabel, dietLabel, from, to, onSuccess, onFailure) {
+      var resource = '/search?q='+query+'&from='+from+'&to='+to
 
       if(healthLabel.length > 0) {
          resource = resource.concat('&health='+healthLabel)
